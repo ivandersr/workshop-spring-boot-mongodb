09d012444afd9190c7b7c62a4624e4c8fe27bdd3
Ivander <ivandersalvadorruiz@gmail.com>
2020-05-26 11:57:51 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/salvadot/workshopmongo/services/UserService.java
+++ src/main/java/com/salvadot/workshopmongo/services/UserService.java
@@ -30,9 +30,16 @@ public class UserService {
 		return repo.insert(user);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO userDTO) {
 		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
+	
+
 
 	
 }
