35c35fbd715bcd6aef11d1efc39768142ed67f2c
Ivander <ivandersalvadorruiz@gmail.com>
2020-05-26 10:25:07 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/salvadot/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.salvadot.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.salvadot.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
