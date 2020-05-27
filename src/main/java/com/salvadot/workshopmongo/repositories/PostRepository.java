980eb8b40fae2d74b8971d76e58701dbf4262c9d
Ivander <ivandersalvadorruiz@gmail.com>
2020-05-27 11:20:27 -0300

    Consulta com vários critérios

--- src/main/java/com/salvadot/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/salvadot/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.salvadot.workshopmongo.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -10,12 +11,13 @@ import com.salvadot.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-	
+
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
-	
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
-	
-	
+
+	@Query("{ $and: [ { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, {'comments.text': { $regex: ?0, $options: 'i' } } ] } , { 'date': { $gte: ?1 } }, { 'date': { $lte: ?2 } } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
