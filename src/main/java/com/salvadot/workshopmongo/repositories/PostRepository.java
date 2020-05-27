df194f3a783f4606af03746735d47dc792886938
Ivander <ivandersalvadorruiz@gmail.com>
2020-05-27 10:40:12 -0300

    Consulta simples com @Query

--- src/main/java/com/salvadot/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/salvadot/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.salvadot.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.salvadot.workshopmongo.domain.Post;
@@ -10,6 +11,11 @@ import com.salvadot.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	
 
 }
